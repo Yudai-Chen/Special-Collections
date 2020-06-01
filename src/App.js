9671f2756ba056536083e52946b2447a3914cdff
Yudai-Chen <yc121@rice.edu>
2020-06-01 04:13:38 -0500

    save before ejecting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TreeView from "react-treeview";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// This example data format is totally arbitrary. No data massaging is
+// required and you use regular js in `render` to iterate through and
+// construct your nodes.
+const dataSource = [
+  ["Apple", "Orange"],
+  ["Facebook", "Google"],
+  ["Celery", "Cheeseburger"],
+];
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collapsedBookkeeping: dataSource.map(() => false),
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.collapseAll = this.collapseAll.bind(this);
+  }
+
+  handleClick(i) {
+    let [...collapsedBookkeeping] = this.state.collapsedBookkeeping;
+    collapsedBookkeeping[i] = !collapsedBookkeeping[i];
+    this.setState({ collapsedBookkeeping: collapsedBookkeeping });
+  }
+
+  collapseAll() {
+    this.setState({
+      collapsedBookkeeping: this.state.collapsedBookkeeping.map(() => true),
+    });
+  }
+
+  async getDataAxios() {
+    const response = await axios.get(
+      "http://jsonplaceholder.typicode.com/users"
+    );
+    console.log(response.data);
+  }
+
+  render() {
+    this.getDataAxios();
+    const collapsedBookkeeping = this.state.collapsedBookkeeping;
+    return (
+      <div>
+        <button onClick={this.collapseAll}>Collapse all</button>
+        {dataSource.map((node, i) => {
+          // Let's make it so that the tree also toggles when we click the
+          // label. Controlled components make this effortless.
+          const label = (
+            <span className="node" onClick={this.handleClick.bind(null, i)}>
+              Type {i}
+            </span>
+          );
+          return (
+            <TreeView
+              key={i}
+              nodeLabel={label}
+              collapsed={collapsedBookkeeping[i]}
+              onClick={this.handleClick.bind(null, i)}
+            >
+              {node.map((entry) => (
+                <div className="info" key={entry}>
+                  {entry}
+                </div>
+              ))}
+            </TreeView>
+          );
+        })}
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/pages/mainpage/components/archive/archive.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Archive extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Hello, World.</p>
+        </header>
+      </div>
+    );
+  }
+}
